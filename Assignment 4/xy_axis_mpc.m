e9a5200662da4e522160f5f7430b8006ffdc9e55
KAN201197 <128454220+KAN201197@users.noreply.github.com>
2023-12-11 13:19:05 +0800

    Assignment 4 (Implementation of MPC)

---
+++ Assignment 4/xy_axis_mpc.m
@@ -0,0 +1,43 @@
+function j = xy_axis_mpc(K, dt, p_0, v_0, a_0, pt, vt, at)
+
+    % Define the maximum/minimum constraint for velocity, acceleration, and jerk
+    vmax = 6.0; % Maximum velocity constraint
+    vmin = 6.0; % Minimum velocity constraint
+    amax = 3.0; % Maximum acceleration constraint
+    amin = 3.0; % Minimum acceleration constraint
+    jerkmax = 3.0; % Maximum jerk constraint
+    jerkmin = 3.0; % Minimum jerk constraint
+    
+    % Construct the prediction matrix for position, velocity, and acceleration
+    [Tp, Tv, Ta, Bp, Bv, Ba] = getPredictionMatrix(K, dt, p_0, v_0, a_0);
+
+    % Define the weight that will be used in the cost function
+    w1 = 100.0; % Weight for tracking error
+    w2 = 1.0; % Weight for velocity 
+    w3 = 1.0; % Weight for acceleration 
+    w4 = 1.0; % Weight for jerk 
+    w5 = 1.0; % Weight for applying soft constraint
+
+    % Construct the optimization problem
+    H = blkdiag(w4 * eye(K) + w1 * (Tp' * Tp) + w2 * (Tv' * Tv) + w3 * (Ta' * Ta), w5 * eye(K));
+    F = [w1 * (Bp - pt)' * Tp + w2 * (Bv - vt)' * Tv + w3 * (Ba - at)' * Ta, zeros(1,K)];
+    
+    % Construct soft constraints
+    A = [Tv, zeros(K);-Tv, -eye(K); Ta, zeros(K);-Ta, zeros(K); zeros(size(Ta)), -eye(K)];
+    b = [vmax * ones(20,1) - Bv; vmin * ones(20,1) + Bv; amax * ones(20,1) - Ba; amin * ones(K,1) + Ba; zeros(K,1)];
+
+    % Define hard constraints on jerk as additional restriction
+    lb = -jerkmin * ones(K,1);
+    ub = jerkmax * ones(K,1);
+
+    % Define linear equality constraints
+    Aeq = []; % not use equality constraint for this case
+    Beq = []; % not use equality constraint for this case
+    
+    % Solve the optimization problem using quadratic programming solver
+    J = quadprog(H, F, A, b, Aeq, Beq, lb, ub);
+  
+    % The output of the desired jerk
+    j = J(1);
+
+end
